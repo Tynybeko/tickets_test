600c15a7b2df7427f4aa196ad275a8e909c4c58c
Tynybek <tynybeko1122@gmail.com>
2024-12-18 00:05:31 +0600

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useReducer, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { ITickets } from './types/type.models';
 import getTickets from './api/get';
 
